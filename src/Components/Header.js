123abd2d28fbba30cdf4a0162df4c5dcddbf3861
user <njkita007@yandex.ru>
2023-02-02 15:26:09 +0300

    Min header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import photo from './Dima_photo.png';
+function Header() {
+    return (
+        <header className='text-white items-center justify-center flex'>
+            <div className="flex container">
+                <div className="w-1/2 p-4 text-left">
+                    <h1 className="text-2xl text-primary">Кооль Дмитрий</h1>
+                    <h2 className='text-4xl'>Квалифицированный .NET разработчик</h2>
+                </div>
+            </div>
+        </header>
+    );
+}
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-global': '#003',
+        'primary': '#676cdb',
+      },
+    },
   },
   plugins: [],
 }
